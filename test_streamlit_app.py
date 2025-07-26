7a1c05dad9f602c673f5b552d7f3117cfc50e0c4
ketand114 <kt.ketand@gmail.com>
2025-07-26 13:04:20 +0100

    Update test_streamlit_app.py

--- test_streamlit_app.py
+++ test_streamlit_app.py
@@ -5,17 +5,21 @@ st.title("Test Streamlit LLM Chat App")
 
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
-def generate_response(input_text):
-    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
-    st.info(model.invoke(input_text))
 
-with st.form("my_form"):
-    text = st.text_area(
-        "Enter text:",
-        "What are the three key pieces of advice for learning how to code?",
-    )
-    submitted = st.form_submit_button("Submit")
-    if not openai_api_key.startswith("sk-"):
-        st.warning("Please enter your OpenAI API key!", icon="⚠")
-    if submitted and openai_api_key.startswith("sk-"):
-        generate_response(text)
+if openai_api_key and openai_api_key.startswith("sk-"):
+    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
+
+    def generate_response(input_text):
+        response = model.invoke(input_text)
+        st.info(response)
+    
+    with st.form("my_form"):
+        text = st.text_area(
+            "Enter text:",
+            "What are the three key pieces of advice for learning how to code?",
+        )
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            generate_response(text)
+else:
+    st.warning("Please enter your OpenAI API key!", icon="⚠")
